d9409d20e5c6ef116e8e560f59b300251937cdce
PoovarasanMoorthy <poovarasanmoorthy375@gmail.com>
2026-03-09 11:41:50 +0530

    optimal

--- src/FindPivotIndex.java
+++ src/FindPivotIndex.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class FindPivotIndex {
+    /*
+    Brute force & time complexity:O(N^2)
     public int pivotIndex(int[] nums) {
+
         for(int index=0;index<nums.length;index++){
             if(isPivot(index,nums)){
                 return index;
@@ -20,11 +25,33 @@ public class FindPivotIndex {
             right+=arr[index];
         }
         return left==right;
+    }*/
+
+    //
+    public int pivotIndex(int[]nums){
+        int[] prefixSum=new int[nums.length];
+        prefixSum[0]=nums[0];
+
+        for(int index=1;index<nums.length;index++){
+            prefixSum[index]=prefixSum[index-1] + nums[index];
+
+        }
+        System.out.println(Arrays.toString(prefixSum));
+        //find the pivot
+        for(int index=0;index<nums.length;index++){
+            int left=(index==0)?0:prefixSum[index-1];
+            int right=prefixSum[nums.length-1]-prefixSum[index];
+            if(left==right){
+                return index;
+            }
+
+        }
+        return -1;
     }
 
-    static void main() {
+    public static void main(String[] a) {
         FindPivotIndex obj=new FindPivotIndex();
-        int[]arr={1,7,3,6,5,6};
+        int[]arr={-1,-1,0,1,1,0};
         System.out.println(obj.pivotIndex(arr));
 
     }
