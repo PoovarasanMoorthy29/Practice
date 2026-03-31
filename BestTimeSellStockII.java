0a56e10706589cdc1fc75fdfe61aa8c58e07b326
PoovarasanMoorthy <poovarasanmoorthy375@gmail.com>
2026-03-31 11:26:09 +0530

    Today leetcode problem updated-Best time to sell stock II

---
+++ BestTimeSellStockII.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BestTimeSellStockII {
+    public int maxProfit(int[] prices) {
+        List<ArrayList<Integer>> profitList=new ArrayList<>();
+        for(int i=0;i<prices.length-1;i++){
+            ArrayList<Integer> currentProfit=new ArrayList<>();
+            for(int j=i+1;j<=i+1;j++){
+                currentProfit.add(prices[j]-prices[i]);
+            }
+            profitList.add(currentProfit);
+        }
+
+        int profit=0;
+        for(int i=0;i<profitList.size();i++){
+            if(profitList.get(i).get(0)>0){
+                profit+=profitList.get(i).get(0);
+            }
+        }
+        return (profit>0)?profit:0;
+    }
+    public static void main(String [] args){
+        int [] arr={7,1,5,3,6,4};
+        BestTimeSellStockII obj=new BestTimeSellStockII();
+        System.out.println(obj.maxProfit(arr));
+    }
+}
