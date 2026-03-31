5a12c39eb02298b1ec7f4b6652551e2444eea074
PoovarasanMoorthy <poovarasanmoorthy375@gmail.com>
2026-03-31 14:03:46 +0530

    extremely optimized  solution updated-Best time to sell stock II

--- BestTimeSellStockII.java
+++ BestTimeSellStockII.java
@@ -13,7 +13,6 @@ public class BestTimeSellStockII {
             }
             profitList.add(currentProfit);
         }
-
         int profit=0;
         for(int i=0;i<profitList.size();i++){
             if(profitList.get(i).get(0)>0){
@@ -23,11 +22,40 @@ public class BestTimeSellStockII {
         return (profit>0)?profit:0;
         */
 
+
+        /*
+        // Better force
+        List<Integer> profitList=new ArrayList<>();
+        for(int i=0;i<prices.length-1;i++){
+            for(int j=i+1;j<=i+1;j++){
+                profitList.add(prices[j]-prices[i]);
+            }
+
+        }
+        int profit=0;
+        for(int i=0;i<profitList.size();i++){
+            if(profitList.get(i)>0){
+                profit+=profitList.get(i);
+            }
+        }
+        return profit;
+        */
+
+        /*
         // optimal solution & Time Complexity :0(N) & space complexity :O(1)
         int profit=0;
         for(int index=0;index<prices.length-1;index++){
             if(prices[index+1]-prices[index] >0){
                 profit+=prices[index+1]-prices[index];
+            }
+        }
+        return profit; */
+        //  optimized to the core
+        int profit=0;
+        for(int index=0;index<prices.length-1;index++){
+            int currentProfit=prices[index+1]-prices[index];
+            if( currentProfit>0){
+                profit+=currentProfit;
             }
         }
         return profit;
