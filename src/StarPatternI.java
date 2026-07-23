95c3bec353658dcf786ebff491990f6e7e858130
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-07-23 21:48:37 +0530

    FIX: set i=1 in the outer loop

--- src/StarPatternI.java
+++ src/StarPatternI.java
@@ -4,13 +4,12 @@ public class StarPatternI {
     public static void main(String[] args) {
         int n = 4;
         int temp = n;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             int val = temp;
             int incr = 0;
             for (int j = 0; j < i; j++) {
                 System.out.print(val + ((j > 0) ? incr : 0) + " ");
                 incr += 2;
-
             }
             temp += i;
             System.out.println();
